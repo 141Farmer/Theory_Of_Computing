c42be4f2036db790c21ea48b8351268b6132b429
141Farmer <rifat24141@gmail.com>
2023-10-05 00:10:01 +0600

    5 10

--- cfg.cpp
+++ cfg.cpp
@@ -8,23 +8,27 @@ bool isPlndrm(string s)
     for(int i=0;i<l/2;++i)  if(s[i]!=s[l-i-1])  return 0;
     return 1;
 }
-void check(string s)
+void check(string s,int begin,int end)
 {
-    int l=s.length();
-    if(s=="") cout<<"P -> epsilon -> "<<endl;
-    else if(l>1 and s[0]=='0' and s[l-1]=='0') cout<<"P-> 0P0 -> "<<s<<endl;
-    else if(l>1 and s[0]=='1' and s[l-1]=='1') cout<<"P -> 1P1 -> "<<s<<endl;
-    else if(l==1 and s[0]=='0')  cout<<"P -> 0 -> "<<s<<endl;
-    else cout<<"P -> 1 -> "<<s<<endl;
+    if(begin>end) 
+    {
+        cout<<s<<endl;
+        return;
+    }
+    for(int i=0;i<begin;++i)  cout<<s[i];
+    if(begin!=end)  cout<<"P";
+    for(int i=end;i<s.length();++i)  cout<<s[i];
+    cout<<"-> ";
+    check(s,begin+1,end-1);
 }
 void input(string &s)
 {
-    if(freopen("cfgInput.txt","r",stdin)==NULL) cout<<"!F\n";
+    if(freopen("cfgInput.txt","r",stdin)==NULL)  cout<<"!F\n";
     while(cin>>s)
     {
         cout<<s<<endl;
-        if(!isPlndrm(s)) cout<<"Not in this grammer\n";
-        else if(isPlndrm(s))  check(s);
+        if(!isPlndrm(s))  cout<<"Not in this grammer\n";
+        else if(isPlndrm(s))  check(s,0,s.length());
     }
 }
 int main()
