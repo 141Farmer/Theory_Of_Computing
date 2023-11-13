d89c347749f57222aeadf89821590266f27e847e
141Farmer <rifat24141@gmail.com>
2023-11-13 23:14:39 +0600

    faith and prayer

--- cfgDerivation.cpp
+++ cfgDerivation.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+
 using namespace std;
+
 int numSymbol;
 vector<string> symbols;
 vector<vector<string>> grammer;
 string s;
+
 void input()
 {
-    if(freopen("cfgDerivation.txt","r",stdin)==NULL) cout<<"!F\n";
+    if(freopen("cfgDerivation.txt","r",stdin)==NULL) 
+        cout<<"!F\n";
     cout<<"Enter number of symbols\n";
     cin>>numSymbol;
     int i;
@@ -31,21 +35,25 @@ void input()
     cout<<"Enter string\n";
     cin>>s;
 }
+
 bool check(string current,int index)
 {
-    if(index>=current.length()) return false;
+    if(index>=current.length()) 
+        return false;
     for(int i=0;i<index;++i)
     {
-        if(current[i]!=s[i]) return false;
+        if(current[i]!=s[i]) 
+            return false;
     }
     return true;
 }
+
 string derivation(string temp)
 {
     string current=temp;
     int l=current.length();
     vector<string>derivations(numSymbol,current);
-    for(int i=0;i<numSymbol;++i)
+    for(int i=1;i<numSymbol;++i)
     {
         string str=symbols[i];
         int idx=derivations[i].find(str);
@@ -62,12 +70,15 @@ string derivation(string temp)
                 }
             }
             idx=derivations[i].find(str,idx+1);
-            current.replace(current.find(symbols[i]),symbols[i].length(),"");
+            //current.replace(current.find(symbols[i]),symbols[i].length(),"");
+            //erase(current,symbols[i][0]);
+            current.erase(remove(current.begin(),current.end(),symbols[i][0]),current.end());
             return current;
         }
     }
     return temp;
 }
+
 void leftDerivation()
 {
     string temp=grammer[0][0];
@@ -84,9 +95,21 @@ void leftDerivation()
         cout<<temp<<endl;
     }
 }
+
+void rightDerivation()
+{
+    string temp=grammer[0][0];
+    cout<<temp<<endl;
+    string revTemp=string(temp.rbegin(),temp.rend());
+    while(temp!=s)
+    {
+        
+    }
+}
+
 int main()
 {
     input();
     leftDerivation();
-    //rightDerivation();
+    rightDerivation();
 }
